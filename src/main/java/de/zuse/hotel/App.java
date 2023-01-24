a7a293e452e4e884fbe1d8ecf4ca6ae0253998e7
baselsaad <basel.swad@gmail.com>
2023-01-25 00:02:11 +0100

    Update

--- src/main/java/de/zuse/hotel/App.java
+++ src/main/java/de/zuse/hotel/App.java
@@ -2,20 +2,14 @@ package de.zuse.hotel;
 
 import de.zuse.hotel.gui.*;
 
-public class App
-{
-	public static void main(String[] args)
-	{
-		Layer layer = new Example();
+public class App {
+    public static void main(String[] args) {
+        Layer layer = new Example();
 
-		{
-			layer.onStart();
-			layer.run(args);
-			layer.onClose();
-		}
+        layer.onStart();
+        layer.run(args);
+        layer.onClose();
+    }
 
-		System.out.println("Github test");
-
-	}
 
 }
