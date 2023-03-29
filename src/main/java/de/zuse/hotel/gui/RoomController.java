ed858afde4be195af7b113d288ba9595263689fa
Jandost-Ahmad <121315436+Jandost-Ahmad@users.noreply.github.com>
2023-03-30 00:18:24 +0200

    tabelviwo

--- src/main/java/de/zuse/hotel/gui/RoomController.java
+++ src/main/java/de/zuse/hotel/gui/RoomController.java
@@ -2,20 +2,18 @@ package de.zuse.hotel.gui;
 
 
 import de.zuse.hotel.core.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -25,6 +23,8 @@ public class RoomController implements ControllerApi {
     TextField roomnumber;
     @FXML
     ChoiceBox<Integer> floorChoiceBox;
+    @FXML
+    TableView<Room> roomTable;
     @FXML
     private TableColumn<Room , Integer> roomNrCln;
     @FXML
