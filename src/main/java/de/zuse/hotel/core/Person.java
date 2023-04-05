1c3500397d506b3b5f52a068f7bd21dbf77af62a
Jandost-Ahmad <121315436+Jandost-Ahmad@users.noreply.github.com>
2023-04-05 21:30:49 +0200

    Regex fix (Basel Buges)

--- src/main/java/de/zuse/hotel/core/Person.java
+++ src/main/java/de/zuse/hotel/core/Person.java
@@ -48,7 +48,7 @@ public class Person {
         ZuseCore.check(address != null, "address can not be null!!");
         ZuseCore.check(is18OrOlder(birthday), "The Person must be 18 years old!!");
 
-        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern pattern = Pattern.compile(emailRegex);
         Matcher matcher = pattern.matcher(email);
         ZuseCore.check(matcher.matches(), "The provided email address is not valid");
